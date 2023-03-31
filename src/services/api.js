7e229bd47587d2827d7e1f37088f0a29da21334b
Thiago Pavan da Silva <96990538+Pavanzinho@users.noreply.github.com>
2023-03-31 17:23:29 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api=axios.create({
-    baseURL:"https://rocketnotes-pavanzinho.onrender.com"
+    baseURL:"https://rocketmovies-pavanzinho.onrender.com"
 })
